b2722ddf8b894d0b496caa23a8f3d3083cf9cabb
jc <624099049@qq.com>
2018-11-13 21:53:01 +0800

    Update 57 cloth-index txt file

--- src/cluster/tools/indexConfirm.py
+++ src/cluster/tools/indexConfirm.py
@@ -8,13 +8,15 @@ import numpy as np
 from keras.preprocessing import image
 
 
-
-
-
+imgnet_txt_path = '/home/jc/codes/Projects/TBCloth/src/cluster/tools/imagenet1000_clsid_to_human.txt'
 cate_txt_path = '/home/jc/codes/Projects/TBCloth/src/classifier/tools/cates.txt'
 test_img_path = '/home/jc/codes/Projects/TBCloth/src/cluster/imgs/3007781.jpg'
 
 def get_model():
+    '''
+    get a vgg16 model fot test
+    :return:
+    '''
     model = VGG16 \
         (include_top=True, weights='imagenet', input_tensor=None,
          input_shape=(224, 224, 3), pooling=None,
@@ -22,14 +24,48 @@ def get_model():
     return model
 
 def main():
-    cates = pd.read_table(cate_txt_path,sep=',')['cate']
+    """
+    Get source categories' indices in image-net-prediction array
+    we get some cloths here , you can get your own list for this
+    :return:
+    """
+    srcCates = pd.read_table(cate_txt_path,sep=',')['cate']
+    allCates = pd.read_table(imgnet_txt_path,sep=':')['content']
+    dic = {}
+    for item in srcCates:
+        i=0
+        for cot in allCates:
+            i+=1
+            if str(cot).find(item)>-1:
+                if(dic.has_key(str(cot))):
+                    print ('Opps dic dump')
+                else:
+                    dic[item] = i
+
+    # You'd better add this row with no space manually:
+    # name,index
+    # for later cluster's work.(or pd.read_table() then add it)
+    pd.Series(dic).sort_values().to_csv('catesIndex.txt')
+
+def model_test():
+    """
+    If you never trust a guy who has no official note,
+    you can have a try of this txt file
+    (I tried 10 times , all works)
+    :return:
+    """
+    cates = pd.read_table(cate_txt_path, sep=',')['cate']
     model = get_model()
     img = image.load_img(test_img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     pred = model.predict(x)
+
+    # The 770 is the index of 'running_shoe' in that txt file
+    # you will find they have the same number (Other also works)
     print(list(pred[0])[770])
-    print(decode_predictions(pred)[0])
+    print(decode_predictions(pred)[0][0])
 
 
 main()
+
